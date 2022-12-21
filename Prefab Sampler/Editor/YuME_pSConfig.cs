61e3e3e1d6dc72d311456a8de3ed9608b135a162
Yuponic <nic.cusworth@gmail.com>
2022-12-21 10:48:27 +0000

    Add files via upload

---
+++ Prefab Sampler/Editor/YuME_pSConfig.cs
@@ -0,0 +1,117 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class YuME_pSConfig : EditorWindow
+{
+    public static YuME_prefabSamplerData editorData;
+
+    [MenuItem("Window/Yuponic/Utils/YuME: Prefab Sampler Config")]
+    static void Initialize()
+    {
+        YuME_pSConfig editorConfig = EditorWindow.GetWindow<YuME_pSConfig>(true, "Prefab Sampler Config");
+        editorConfig.titleContent.text = "Prefab Sampler Config";
+    }
+
+    void OnEnable()
+    {
+        editorData = ScriptableObject.CreateInstance<YuME_prefabSamplerData>();
+        string[] guids = AssetDatabase.FindAssets("YuME_prefabSamplerSettings");
+
+        if (guids.Length == 0)
+        {
+            // create a new setting file
+            Debug.Log("Yuponic Prefab Sampler: No settings file found. Creating new settings file");
+            editorData = ScriptableObject.CreateInstance<YuME_prefabSamplerData>();
+
+            AssetDatabase.CreateAsset(editorData, "Assets/Yuponic/YuME_prefabSamplerSettings.asset");
+            AssetDatabase.SaveAssets();
+
+            editorData = AssetDatabase.LoadAssetAtPath("Assets/Yuponic/YuME_prefabSamplerSettings.asset", typeof(YuME_prefabSamplerData)) as YuME_prefabSamplerData;
+        }
+        else
+        {
+            // load the settings
+            editorData = AssetDatabase.LoadAssetAtPath(AssetDatabase.GUIDToAssetPath(guids[0]), typeof(YuME_prefabSamplerData)) as YuME_prefabSamplerData;
+        }
+
+        checkForValidSettings();
+    }
+
+    void checkForValidSettings()
+    {
+        if (editorData.yPivotTypes.Length < 3)
+        {
+            editorData.yPivotTypes = new string[3];
+            editorData.yPivotTypes[0] = "Zero Y";
+            editorData.yPivotTypes[1] = "Selection Ground";
+            editorData.yPivotTypes[2] = "Selection Middle";
+        }
+
+        if(editorData.destinationFolder == "")
+        {
+            editorData.destinationFolder = "Assets/";
+        }
+
+        if (editorData.destinationFolder != "Assets/")
+        {
+            if (!AssetDatabase.IsValidFolder(editorData.destinationFolder))
+            {
+                editorData.destinationFolder = "Assets/";
+            }
+        }
+    }
+
+    void OnGUI()
+    {
+        EditorGUILayout.Space();
+        
+        EditorGUILayout.BeginVertical("box");
+
+        EditorGUILayout.LabelField("Select Prefab Destination Folder", EditorStyles.boldLabel);
+
+        if (GUILayout.Button("Prefab Destination Folder", GUILayout.Height(30)))
+        {
+            editorData.destinationFolder = EditorUtility.OpenFolderPanel("Prefab Destination Folder", "", "");
+            editorData.destinationFolder = YuTools_Utils.shortenAssetPath(editorData.destinationFolder);
+            if (editorData.destinationFolder == "")
+            {
+                editorData.destinationFolder = "Assets/";
+            }
+        }
+
+        EditorGUILayout.BeginVertical("box");
+
+        EditorGUILayout.LabelField("Prefab Folder:", EditorStyles.boldLabel);
+        EditorGUILayout.LabelField(editorData.destinationFolder);
+
+        EditorGUILayout.EndVertical();
+        EditorGUILayout.EndVertical();
+
+        EditorGUILayout.BeginVertical("box");
+
+        EditorGUILayout.LabelField("Prefab Pivot Setting:", EditorStyles.boldLabel);
+        editorData.yPivotType = EditorGUILayout.Popup("Pivot Setting", editorData.yPivotType, editorData.yPivotTypes);
+        EditorGUILayout.EndVertical();
+
+        // ----------------------------------------------------------------------------------------------------
+        // ------ Append Settings
+        // ----------------------------------------------------------------------------------------------------
+
+        EditorGUILayout.BeginVertical("box");
+
+        EditorGUILayout.LabelField("Custom Append Label", EditorStyles.boldLabel);
+
+        EditorGUILayout.BeginVertical("box");
+
+        editorData.appendName = EditorGUILayout.TextField("Append to Prefab Name", editorData.appendName);
+
+        EditorGUILayout.EndVertical();
+        EditorGUILayout.EndVertical();
+
+        if (GUI.changed)
+        {
+            SceneView.RepaintAll();
+        }
+
+    }
+}
---
+++ Prefab Sampler/Editor/YuME_prefabSampler.cs
@@ -0,0 +1,143 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+using UnityEditorInternal;
+
+class YuME_prefabSampler : EditorWindow
+{
+    List<GameObject> selected = new List<GameObject>();
+    public static YuME_prefabSamplerData editorData;
+    static bool openConfig = false;
+
+    [MenuItem("Window/Yuponic/YuME: Prefab Sampler")]
+    static void Initialize()
+    {
+        YuME_prefabSampler prefabSamplerWindow = EditorWindow.GetWindow<YuME_prefabSampler>(false, "YuME: Prefab Sampler");
+        prefabSamplerWindow.titleContent.text = "YuME: Prefab Sampler";
+    }
+
+    void OnEnable()
+    {
+        editorData = ScriptableObject.CreateInstance<YuME_prefabSamplerData>();
+        string[] guids = AssetDatabase.FindAssets("YuME_prefabSamplerSettings");
+        editorData = AssetDatabase.LoadAssetAtPath(AssetDatabase.GUIDToAssetPath(guids[0]), typeof(YuME_prefabSamplerData)) as YuME_prefabSamplerData;
+    }
+
+    void OnGUI()
+    {
+        EditorGUILayout.Space();
+
+        if (editorData.destinationFolder != "" )
+        {
+            EditorGUILayout.BeginVertical("box");
+
+            EditorGUILayout.BeginHorizontal();
+            if (GUILayout.Button("SAMPLE PREFAB", GUILayout.Height(30)))
+            {
+                if (selected.Count > 0)
+                {
+                    samplePrefab();
+                }
+            }
+            openConfig = GUILayout.Toggle(openConfig, editorData.configButton, "Button", GUILayout.Width(30), GUILayout.Height(30));
+
+            if (openConfig == true)
+            {
+                YuME_pSConfig editorConfig = EditorWindow.GetWindow<YuME_pSConfig>(true, "Prefab Sampler Config");
+                editorConfig.titleContent.text = "Prefab Sampler Config";
+            }
+
+            openConfig = false;
+            EditorGUILayout.EndHorizontal();
+            EditorGUILayout.EndVertical();
+
+            EditorGUILayout.BeginVertical("box");
+
+            EditorGUILayout.LabelField("Prefab Folder:", EditorStyles.boldLabel);
+            EditorGUILayout.LabelField(editorData.destinationFolder);
+
+            EditorGUILayout.EndVertical();
+        }
+        else
+        {
+            EditorGUILayout.HelpBox("Set a destination directory to create prefabs", MessageType.Warning);
+        }
+    }
+
+    void OnSelectionChange()
+    {
+        selected.Clear();
+
+        foreach (GameObject selection in Selection.gameObjects)
+        {
+            selected.Add(selection);
+        }
+    }
+
+    void samplePrefab()
+    {
+        GameObject prefabParent = new GameObject("Parent Object");
+        var myBounds = InternalEditorUtility.CalculateSelectionBounds(false, false);
+        Vector3 centerPoint = myBounds.center;
+
+        switch(editorData.yPivotType)
+        {
+            case 0:
+                centerPoint.y = 0;
+                break;
+
+            case 1:
+                centerPoint.y = myBounds.min.y;
+                break;
+
+            case 2:
+                centerPoint.y = myBounds.center.y;
+                break;
+
+            default:
+                centerPoint.y = 0;
+                break;
+        }
+
+        prefabParent.transform.position = centerPoint;
+
+        foreach(GameObject copyObject in selected)
+        {
+            GameObject copy = Instantiate(copyObject) as GameObject;
+            copy.transform.position = copyObject.transform.position;
+            copy.transform.eulerAngles = copyObject.transform.eulerAngles;
+            copy.transform.parent = prefabParent.transform;
+        }
+
+        prefabParent.name = editorData.appendName + selected[0].name;
+        prefabParent.transform.position = Vector3.zero;
+
+#if UNITY_2018_3_OR_NEWER
+        PrefabUtility.SaveAsPrefabAsset(prefabParent, appendNumber(editorData.destinationFolder + "/" + prefabParent.name));
+#else
+        PrefabUtility.CreatePrefab(appendNumber(editorData.destinationFolder + "/" + prefabParent.name), prefabParent);
+#endif
+
+        DestroyImmediate(prefabParent);
+    }
+
+    string appendNumber(string asset)
+    {
+        int uniqueNumber = 0;
+        bool foundUnique = false;
+        while (!foundUnique)
+        {
+            Object prefabAlreadyCreated = AssetDatabase.LoadAssetAtPath(asset+uniqueNumber.ToString()+".prefab", typeof(GameObject));
+            if (prefabAlreadyCreated == null)
+            {
+                foundUnique = true;
+            }
+            else
+            {
+                uniqueNumber++;
+            }
+        }
+
+        return asset + uniqueNumber.ToString() + ".prefab";
+    }
+}
